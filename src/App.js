d3d43d5bbcce6af024f1dc1530638f133ce01063
jstabi01 <65237420+jstabi01@users.noreply.github.com>
2022-03-03 17:51:15 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Eightball from './EightBall';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Eightball />
+    </div>
+  );
+}
+
+export default App;
